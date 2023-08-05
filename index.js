718716a7ed5e18785983c86f7a1141fc513458bb
5E6-Aishwarya <20wh1a05e6@bvrithyderabad.edu.in>
2023-08-05 10:15:50 +0530

    update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@
 var express = require("express");  
 var app = express();  
 
-app.set("views" + __dirname + "/views")
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine","ejs");
 
 const { initializeApp, cert } = require("firebase-admin/app");
